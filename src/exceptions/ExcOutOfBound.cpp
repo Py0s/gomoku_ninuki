a945132eb0aebc359afc1340bcd40b4aa5302586
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-11-25 19:02:34 +0100

    Commit de merde

--- src/exceptions/ExcOutOfBound.cpp
+++ src/exceptions/ExcOutOfBound.cpp
@@ -1,12 +1,12 @@
 #include "ExcOutOfBound.h"
 
-ExcOutOfBound::ExcOutOfBound(): Exceptions('Out of boundaries') {
+ExcOutOfBound::ExcOutOfBound(): Exceptions("Out of boundaries") {
 }
 
 ExcOutOfBound::~ExcOutOfBound() {
 }
 
 // Members
-const int Exceptions::errNo() const throw () {
+const int ExcOutOfBound::errNo() const throw () {
     return 2;
 }
