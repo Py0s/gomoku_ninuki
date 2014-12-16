52872efda811580c2e55c6699263c02d21c341bd
marie <marie.lefebvre@epitech.eu>
2014-12-16 10:26:16 +0100

    refactoring guis dans game

--- src/game/Game.h
+++ src/game/Game.h
@@ -11,6 +11,11 @@
 
 class Game {
 public:
+    enum E_GUI_STATE {
+        MENU = 0,
+        GAME,
+    };
+
     Game();
     virtual ~Game();
     
@@ -20,28 +25,36 @@ public:
 private:
     Map         _map;
     EventManager    _events;
-//    Core        _core;
     InitSfml    _init_sfml;
-//    AGui*       _guis[2];
-    AGui*       _gui; // Current Gui
-    Menu*       _menu;
+    AGui*       _guis[2];
+    E_GUI_STATE _guiState;
+
     APlayer *   _currentPlayer;
     APlayer *   _players[2] = {NULL, NULL};
     int         _player_nb;
     Config      _conf;
     Referee     _referee;
-    Referee::E_STATE    _currentState;
+    Referee::E_STATE    _gameState;
     
     inline bool quit() const {
         return _events.getKey(EventManager::E_KEYS::QUIT);
     }
 
-    int menu();
+    inline AGui*    gui() { return _guis[static_cast<int>(_guiState)]; }
+    inline Menu*    menu() {
+        assert(_guiState == MENU);
+        return dynamic_cast<Menu*>(gui());
+    }
+    inline void     switchGuiState() {
+        _guiState = (_guiState == MENU) ? (GAME) : (MENU);
+    }
+
+    int menuLoop();
     int start();
     int cleanGame();
     void initPlayers();
     inline void nextPlayer();
     void accept();
-    inline bool gameHasEnded() const { return Referee::gameHasEnded(_currentState); }
+    inline bool gameHasEnded() const { return Referee::gameHasEnded(_gameState); }
 };
 
